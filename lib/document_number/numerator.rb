6c3d7b026c8f1d14e721aa42440df31771e85fa8
Evgeny Pavlov <evgeny@pavlov.name>
2014-06-19 14:03:34 +0400

    Fixed tests
    Check Rails version

--- lib/document_number/numerator.rb
+++ lib/document_number/numerator.rb
@@ -4,8 +4,12 @@ module DocumentNumber
   class Numerator
     def self.next_number(object, options)
       DocumentNumber.transaction do
-        # Rails 3 support
-        document_number = DocumentNumber.lock(true).find_or_initialize_by_document(object.class.to_s.underscore)
+        if ::Rails.version < "4.0"
+          # Rails 3 support
+          document_number = DocumentNumber.lock(true).find_or_initialize_by_document(object.class.to_s.underscore)
+        else
+          document_number = DocumentNumber.lock(true).find_or_initialize_by(:document => object.class.to_s.underscore)
+        end
 
         number = document_number.number == 1 ? options[:start] : document_number.number
 
