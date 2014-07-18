f0367cd0d7e944add6dbbdb14b4fda61edb5a6c2
Evgeny Pavlov <evgeny@pavlov.name>
2014-07-18 23:46:07 +0400

    Tests for new features

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -21,4 +21,16 @@ describe Inventory do
     expect(Inventory.create.number).to eq('1')
     expect(Inventory.create.number).to eq('2')
   end
+
+  it 'gets array of numbers' do
+    expect(Inventory.get_numbers(3)).to eq(%w(1 2 3))
+  end
+
+  it 'assigns number after initialization if has with_number' do
+    expect(Inventory.new(with_number: true).number).to eq('1')
+  end
+
+  it 'does not assign number after initialization' do
+    expect(Inventory.new.number).to be_nil
+  end
 end
--- spec/models/invoices_spec.rb
+++ spec/models/invoices_spec.rb
@@ -5,4 +5,16 @@ describe Invoice do
     invoice = Invoice.create
     expect(invoice.number).to eq('invoice/1')
   end
+
+  it 'gets array of numbers with prefix' do
+    expect(Invoice.get_numbers(3)).to eq(%w(invoice/1 invoice/2 invoice/3))
+  end
+
+  it 'assigns number after initialization if has with_number' do
+    expect(Invoice.new(with_number: true).number).to eq('invoice/1')
+  end
+
+  it 'does not assign number after initialization' do
+    expect(Invoice.new.number).to be_nil
+  end
 end
